659370df8e052f784257e03ce76d0b0557756cb8
winternen <v.wntrn@gmail.com>
2023-04-07 20:06:31 +0300

    add gendiff helper

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.',
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    args = parser.parse_args()
+
+
+
+if __name__ == '__main__':
+    main()
