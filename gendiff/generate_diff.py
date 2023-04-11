5415bfe7ad6c04e21cc8b391410677b095bc7cbc
winternen <v.wntrn@gmail.com>
2023-04-11 10:55:40 +0300

    update generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -8,20 +8,16 @@ def normalize_string(value):
 def generate_diff(first_file, second_file):
     first_dict = json.load(open(first_file))
     second_dict = json.load(open(second_file))
-
-    diff_list = []
+    diff = []
     keys = sorted(set(first_dict.keys()) | set(second_dict.keys()))
-
     for key in keys:
         if key not in first_dict:
-            diff_list.append(f'+ {key}: {normalize_string(second_dict[key])}')
+            diff.append(f'+ {key}: {normalize_string(second_dict[key])}')
         elif key not in second_dict:
-            diff_list.append(f'- {key}: {normalize_string(first_dict[key])}')
+            diff.append(f'- {key}: {normalize_string(first_dict[key])}')
         elif first_dict[key] == second_dict[key]:
-            diff_list.append(f'  {key}: {normalize_string(first_dict[key])}')
+            diff.append(f'  {key}: {normalize_string(first_dict[key])}')
         else:
-            diff_list.append(f'- {key}: {normalize_string(first_dict[key])}')
-            diff_list.append(f'+ {key}: {normalize_string(second_dict[key])}')
-    
-    diff_string = '\n'.join(diff_list)
-    return '{\n' + diff_string + '\n}'
+            diff.append(f'- {key}: {normalize_string(first_dict[key])}')
+            diff.append(f'+ {key}: {normalize_string(second_dict[key])}')
+    return '{\n' + '\n'.join(diff) + '\n}'
